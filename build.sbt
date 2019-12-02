2bada62d130ac4c3f29674f990579405cd376f0d
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-12-02 12:28:21 +0100

    day 1

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "adventOfCode2019"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/aof/Day1.scala
@@ -0,0 +1,41 @@
+package aof
+
+import scala.io.Source
+
+object Day1 extends App {
+
+  val rs = getClass.getClassLoader.getResource("input/mass.txt")
+
+  val lines = Source.fromFile(rs.toURI).getLines().toList
+
+  def fuel(mass: Int): Int = mass / 3 - 2
+
+  val res1 = lines.map(_.toInt).foldLeft(0)((acc, m) => acc + fuel(m))
+
+
+  println("checks:")
+  List(12, 14, 1969, 100756).foreach { m =>
+    println(s"mass : $m => " + fuel(m))
+  }
+  println("part1: " + res1)
+
+  def streamOfFuel(m: Int): List[Int] = {
+    val x = fuel(m)
+    if (x <= 0) {
+      List.empty
+    } else {
+      x :: streamOfFuel(x)
+    }
+  }
+
+  println("checks:")
+  List(12, 14, 1969, 100756).foreach { m =>
+    val x = streamOfFuel(m)
+    println(s"mass : $m => $x (" + x.sum + ")")
+  }
+
+  val res2 = lines.map(_.toInt).flatMap(streamOfFuel).sum
+
+  println("part2: " + res2)
+
+}
