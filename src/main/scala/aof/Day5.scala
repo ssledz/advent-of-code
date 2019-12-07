49c48000d742ed113bb127e3c2cba58ef4a9a0af
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-12-07 22:13:09 +0100

    Day5 partA

--- src/main/scala/aof/Day5.scala
+++ src/main/scala/aof/Day5.scala
@@ -1,5 +1,7 @@
 package aof
 
+import aof.IntComputer.opcode
+
 object Day5 extends Day {
 
   val day: String = "05"
@@ -18,46 +20,89 @@ object Day5 extends Day {
 
 }
 
-class IntComputer(m: Array[Int], input: Int, output: Int = 0) {
+class IntComputer(m: Array[Int], input: Int, output: Int = 0, debug: Boolean = false, trace: Boolean = false) {
+
+  def tr(s: String): Unit = if (trace) println("tr: " + s)
+
+  def dbg(s: String): Unit = if (debug) println("dbg: " + s)
 
   def out: Int = m(output)
-
-  def in: Int = m(input)
 
   def runInterpreter: Array[Int] = run(0)
 
   private def run(pc: Int): Array[Int] = {
 
-    def arthmeticOp(f: (Int, Int) => Int): Unit = {
-      val posA = m(pc + 1)
-      val posB = m(pc + 2)
-      val dest = m(pc + 3)
-      m(dest) = f(m(posA), m(posB))
+    tr(s"pc=$pc, memory=${m.toList}")
+
+    def readValue(mode: Int, address: Int): Int =
+      if (mode == 0) m(m(address)) else m(address)
+
+    def arthmeticOp(mode: (Int, Int, Int), f: (Int, Int) => Int): Unit = mode match {
+      case (_, b, c) =>
+        val x = readValue(c, pc + 1)
+        val y = readValue(b, pc + 2)
+        val dest = m(pc + 3)
+        m(dest) = f(x, y)
+        tr(s"op: m($dest) = $x ? $y = ${f(x, y)}")
     }
 
-    val opCode = m(pc)
+    val opc = opcode(m(pc))
 
-    opCode match {
-      case 1 => arthmeticOp(_ + _)
+    tr(s"opcode: $opc")
+
+    opc match {
+      case (Some(mode), 1) => arthmeticOp(mode, _ + _)
         run(pc + 4)
-      case 2 => arthmeticOp(_ * _)
+      case (Some(mode), 2) => arthmeticOp(mode, _ * _)
         run(pc + 4)
-      case 3 =>
-        m(m(pc + 1)) = in
-        run(2)
-      case 4 =>
-        m(output) = m(m(pc + 1))
-        run(2)
-      case 99 => m
-      case _ => throw new IllegalStateException(s"Illegal op code=$opCode, pc=$pc")
+      case (mode, 3) =>
+        m(m(pc + 1)) = input
+        run(pc + 2)
+      case (mode, 4) =>
+        dbg(s"previous test result (pc=$pc): " + output)
+        mode match {
+          case Some((_, _, 0)) => m(output) = m(m(pc + 1))
+          case Some((_, _, 1)) => m(output) = m(pc + 1)
+        }
+        run(pc + 2)
+      case (_, 99) => m
+      case oc => throw new IllegalStateException(s"Illegal op code=$oc, pc=$pc")
     }
   }
+
+}
+
+object IntComputer {
+
+  private def toInt(c: Char): Int = c - '0'
+
+  def opcode(n: Int) = {
+    if (n == 3 || n == 99) {
+      (None, n)
+    } else {
+      n.toString.toList match {
+        case a :: b :: c :: d :: e :: Nil => (Some((toInt(a), toInt(b), toInt(c))), ("" + d + e).toInt)
+        case b :: c :: d :: e :: Nil => (Some(0, toInt(b), toInt(c)), ("" + d + e).toInt)
+        case c :: d :: e :: Nil => (Some(0, 0, toInt(c)), ("" + d + e).toInt)
+        case '1' :: Nil => (Some(0, 0, 0), 1)
+        case '2' :: Nil => (Some(0, 0, 0), 2)
+        case '4' :: Nil => (Some(0, 0, 0), 4)
+      }
+    }
+  }
+
 
 }
 
 object Day5App extends App {
 
   println("SolutionPartA: " + Day5.solutionPartA)
-  println("SolutionPartB: " + Day5.solutionPartB)
+  //  println("SolutionPartB: " + Day5.solutionPartB)
 
+  //    val memory = Array(1002, 4, 3, 4, 33)
+  //  val memory = Array(1101, 100, -1, 4, 0)
+  //  val memory = Array(3, 0, 4, 0, 99)
+  //  val c = new IntComputer(memory, 11)
+  //  c.runInterpreter
+  //  println(memory.toList)
 }
