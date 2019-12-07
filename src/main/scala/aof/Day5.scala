510ea561a4dbf5892aebca66c8ced50cfc8805a7
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-12-07 12:28:33 +0100

    bzz

---
+++ src/main/scala/aof/Day5.scala
@@ -0,0 +1,23 @@
+package aof
+
+object Day5 extends Day {
+  override val day: String = "05"
+
+  val memory: Array[Int] = lines.head.split(',').map(_.toInt)
+
+  def solutionPartA: String = {
+    ""
+  }
+
+  def solutionPartB: String = {
+    ""
+  }
+
+}
+
+object Day5App extends App {
+
+  println("SolutionPartA: " + Day5.solutionPartA)
+  println("SolutionPartB: " + Day5.solutionPartB)
+
+}
