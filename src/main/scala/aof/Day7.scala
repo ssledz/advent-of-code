f2eb376bd29b09c13a5b9646df1860c5d603e549
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-12-09 00:56:28 +0100

    Day7 partA

--- src/main/scala/aof/Day7.scala
+++ src/main/scala/aof/Day7.scala
@@ -1,10 +1,33 @@
 package aof
 
 object Day7 extends Day {
-  
+
   val day: String = "07"
 
-  def solutionPartA: String = ""
+  def memory: Array[Int] = lines.head.split(',').map(_.toInt)
+
+  def runAmplifier(memory: Array[Int], inA: Int, inB: Int): Int = {
+    val c = new IntComputer(memory, List(inA, inB), 0, false, false)
+    c.runInterpreter
+    c.out
+  }
+
+
+  def maxThrusterSignal(memory: () => Array[Int]): (Int, Seq[Int]) = {
+
+    def connectAndRun(xs: Seq[Int]): Int =
+      xs.foldLeft(0)((inA, inB) => runAmplifier(memory(), inB, inA))
+
+    val r = 0 to 4
+
+    r.permutations.map { p =>
+      (connectAndRun(p), p)
+    }.maxBy(_._1)
+
+  }
+
+
+  def solutionPartA: String = "" + maxThrusterSignal(() => memory)._1
 
   def solutionPartB: String = ""
 
--- src/test/scala/aof/Day7Test.scala
+++ src/test/scala/aof/Day7Test.scala
@@ -1,11 +1,23 @@
 package aof
 
+import aof.Day7.maxThrusterSignal
+import aof.Day7Test._
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day7Test extends AnyFunSuite {
 
-  test("") {
-    
+  test("maxThrusterSignal") {
+    assert(maxThrusterSignal(() => Ex1) === (43210, Seq(4, 3, 2, 1, 0)))
+    assert(maxThrusterSignal(() => Ex2) === (54321, Seq(0, 1, 2, 3, 4)))
+    assert(maxThrusterSignal(() => Ex3) === (65210, Seq(1, 0, 4, 3, 2)))
   }
+
+}
+
+object Day7Test {
+
+  def Ex1 = Array(3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0)
+  def Ex2 = Array(3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0)
+  def Ex3 = Array(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0)
 
 }
