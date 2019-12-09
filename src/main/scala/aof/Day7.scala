3401233096acf67b453d16b2d7eae172c34ca649
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-12-09 01:33:37 +0100

    Day7 partB

--- src/main/scala/aof/Day7.scala
+++ src/main/scala/aof/Day7.scala
@@ -6,14 +6,44 @@ object Day7 extends Day {
 
   def memory: Array[Int] = lines.head.split(',').map(_.toInt)
 
+  def createComputer(memory: Array[Int], inA: Int, inB: Int): IntComputer =
+    new IntComputer(memory, List(inA, inB), 0, false, false)
+
   def runAmplifier(memory: Array[Int], inA: Int, inB: Int): Int = {
-    val c = new IntComputer(memory, List(inA, inB), 0, false, false)
+    val c = createComputer(memory, inA, inB)
     c.runInterpreter
     c.out
   }
 
 
-  def maxThrusterSignal(memory: () => Array[Int]): (Int, Seq[Int]) = {
+  def maxThrusterSignalForLooped(memory: () => Array[Int]): (Int, Seq[Int]) = {
+
+    def connectAndRun(xs: Seq[Int]): Int = {
+
+      val memories = xs.map(_ -> memory())
+
+      def go(in: Int, acc: Int): Int = {
+        if (in == 0 && acc > 0) {
+          acc
+        } else {
+          val signal = memories.foldLeft(in) { case (inA, (inB, memory)) => runAmplifier(memory, inB, inA) }
+          go(signal, if (signal == 0) acc else signal)
+        }
+      }
+
+      go(0, 0)
+
+    }
+
+    val r = 5 to 9
+
+    r.permutations.map { p =>
+      (connectAndRun(p), p)
+    }.maxBy(_._1)
+
+  }
+
+  def maxThrusterSignalForSerial(memory: () => Array[Int]): (Int, Seq[Int]) = {
 
     def connectAndRun(xs: Seq[Int]): Int =
       xs.foldLeft(0)((inA, inB) => runAmplifier(memory(), inB, inA))
@@ -27,9 +57,9 @@ object Day7 extends Day {
   }
 
 
-  def solutionPartA: String = "" + maxThrusterSignal(() => memory)._1
+  def solutionPartA: String = "" + maxThrusterSignalForSerial(() => memory)._1
 
-  def solutionPartB: String = ""
+  def solutionPartB: String = "" + maxThrusterSignalForLooped(() => memory)._1
 
 }
 
--- src/test/scala/aof/Day7Test.scala
+++ src/test/scala/aof/Day7Test.scala
@@ -1,23 +1,32 @@
 package aof
 
-import aof.Day7.maxThrusterSignal
+import aof.Day7.{maxThrusterSignalForLooped, maxThrusterSignalForSerial}
 import aof.Day7Test._
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day7Test extends AnyFunSuite {
 
-  test("maxThrusterSignal") {
-    assert(maxThrusterSignal(() => Ex1) === (43210, Seq(4, 3, 2, 1, 0)))
-    assert(maxThrusterSignal(() => Ex2) === (54321, Seq(0, 1, 2, 3, 4)))
-    assert(maxThrusterSignal(() => Ex3) === (65210, Seq(1, 0, 4, 3, 2)))
+  test("maxThrusterSignalForSerial") {
+    assert(maxThrusterSignalForSerial(() => Ex1) === (43210, Seq(4, 3, 2, 1, 0)))
+    assert(maxThrusterSignalForSerial(() => Ex2) === (54321, Seq(0, 1, 2, 3, 4)))
+    assert(maxThrusterSignalForSerial(() => Ex3) === (65210, Seq(1, 0, 4, 3, 2)))
   }
 
+  test("maxThrusterSignalForLooped") {
+    assert(maxThrusterSignalForLooped(() => Ex4) === (139629729, Seq(9, 8, 7, 6, 5)))
+    assert(maxThrusterSignalForLooped(() => Ex5) === (18216, Seq(9, 7, 8, 5, 6)))
+  }
 }
 
 object Day7Test {
 
   def Ex1 = Array(3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0)
+
   def Ex2 = Array(3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0)
+
   def Ex3 = Array(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0)
 
+  def Ex4 = Array(3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5)
+
+  def Ex5 = Array(3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10)
 }
