513b0f74c439017ea65e510f72213964589d1d21
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-12-16 03:04:30 +0100

    Day12 partB

--- src/main/scala/aof/Day12.scala
+++ src/main/scala/aof/Day12.scala
@@ -1,80 +1,95 @@
 package aof
 
+/**
+ *
+ * https://en.wikipedia.org/wiki/Least_common_multiple
+ *
+ * Planetary alignment
+ * Suppose there are three planets revolving around a star which take l, m and n units of time respectively to
+ * complete their orbits. Assume that l, m and n are integers. Assuming the planets started moving around the star
+ * after an initial linear alignment, all the planets attain a linear alignment again after LCM(l, m, n) units of time.
+ * At this time, the first, second and third planet will have completed LCM(l, m, n)/l, LCM(l, m, n)/m and LCM(l, m, n)/n
+ * orbits respectively around the star.[4]
+ *
+ */
 object Day12 extends Day {
 
   val day: String = "12"
 
   lazy val moons = lines.map(Moon.from)
 
-  def gravityChange(a: Int, b: Int): (Int, Int) =
-    if (a < b) (1, -1) else if (a > b) (-1, 1) else (0, 0)
+  val Zero = Vector3d(0, 0, 0)
 
-  def gravityChange(a: Moon, b: Moon): (Vector3d, Vector3d) = {
-    val (xa, xb) = gravityChange(a.pos.x, b.pos.x)
-    val (ya, yb) = gravityChange(a.pos.y, b.pos.y)
-    val (za, zb) = gravityChange(a.pos.z, b.pos.z)
-    (Vector3d(xa, ya, za), Vector3d(xb, yb, zb))
-  }
+  def g(a: Long, b: Long): Int = if (a < b) 1 else if (a > b) -1 else 0
 
-  def pairs[A](xs: Seq[A]): Seq[(A, A)] = {
-    val ys = for {
-      x <- xs
-      y <- xs if x != y
-    } yield (x, y)
-    ys.foldLeft(Seq.empty[(A, A)]) { case (acc, p@(x, y)) =>
-      if (acc contains(y, x)) {
-        acc
-      } else acc :+ p
-    }
-  }
-
-  def applyGravity(xs: Seq[Moon]): Seq[Moon] = {
-
-    val ys = pairs(xs).map { case (a, b) =>
-      val (dva, dvb) = gravityChange(a, b)
-      List(a -> dva, b -> dvb)
-    }
-
-    val yys = ys.flatten.groupBy(_._1).toList.map { case (moon, zs) =>
-      val dv = zs.map(_._2).foldLeft(Vector3d(0, 0, 0))(_ + _)
-      moon.copy(velocity = moon.velocity + dv)
-    }
-    yys
-  }
-
-  def applyVelocity(xs: Seq[Moon]): Seq[Moon] =
-    xs.map { moon =>
-      moon.copy(pos = moon.pos + moon.velocity)
-    }
+  def gc(a: Moon, b: Moon): Vector3d = Vector3d(g(a.pos.x, b.pos.x), g(a.pos.y, b.pos.y), g(a.pos.z, b.pos.z))
 
   def step(xs: Seq[Moon]): Seq[Moon] = {
-    val f = applyGravity _ andThen applyVelocity
-    f(xs)
+    val dv = xs.map(x => xs.filter(_ != x).map(y => gc(x, y)).foldLeft(Zero)(_ + _))
+    xs.zip(dv).map { case (moon, d) =>
+      val v = moon.velocity + d
+      moon.copy(pos = moon.pos + v, velocity = v)
+    }
   }
 
   def steps(xs: Seq[Moon], n: Int): Seq[Moon] =
     (1 to n).foldLeft(xs)((ms, _) => step(ms).toList)
 
-  def totalEnergy(xs: Seq[Moon]): Int =
-    xs.foldLeft(0)((acc, m) => acc + m.totalEnergy)
+  def totalEnergy(xs: Seq[Moon]): Long = xs.foldLeft(0L)((acc, m) => acc + m.totalEnergy)
 
-  def solutionPartA: String = {
-    val xs = steps(moons, 1000)
-    "" + totalEnergy(xs)
+  def solutionPartA: String = "" + totalEnergy(steps(moons, 1000))
+
+  def gcd(a: BigInt, b: BigInt): BigInt = if (b == 0) a else gcd(b, a % b)
+
+  def lcm(a: BigInt, b: BigInt): BigInt = (a * b) / gcd(a, b)
+
+  val n = 3
+
+  def reachInitialState(xs: Seq[Moon]): BigInt = {
+
+    def go(ms: Seq[Moon], iter: Long = 0, state: Seq[Set[Seq[Int]]] = Seq.fill(n)(Set.empty), acc: Seq[Long] = Seq.fill(n)(0)): Seq[Long] = {
+      if (acc.forall(_ != 0)) {
+        acc
+      } else {
+
+        val ns = step(ms)
+
+        val ys = acc.zipWithIndex.zip(state).map { case ((n, i), s) =>
+
+          if (n == 0) {
+            val xs = ns.map(x => x.pos.toVec(i)) ++ ns.map(x => x.velocity.toVec(i))
+            if (s.contains(xs)) (iter, s + xs) else (n, s + xs)
+          } else (n, s)
+
+        }
+
+        go(ns, iter + 1, ys.map(_._2), ys.map(_._1))
+
+      }
+
+    }
+
+
+    go(xs).map(BigInt.apply).iterator.reduce(lcm)
+
   }
 
-  def solutionPartB: String = ""
+  def solutionPartB: String = "" + reachInitialState(moons)
+
 
   case class Vector3d(x: Int, y: Int, z: Int) {
     def +(other: Vector3d): Vector3d = Vector3d(x + other.x, y + other.y, z + other.z)
+
+    def toVec: Vector[Int] = Vector(x, y, z)
   }
 
   case class Moon(pos: Vector3d, velocity: Vector3d = Vector3d(0, 0, 0)) {
-    def pot: Int = Math.abs(pos.x) + Math.abs(pos.y) + Math.abs(pos.z)
 
-    def kin: Int = Math.abs(velocity.x) + Math.abs(velocity.y) + Math.abs(velocity.z)
+    def pot: Long = pos.x.abs + pos.y.abs + pos.z.abs
 
-    def totalEnergy: Int = pot * kin
+    def kin: Long = velocity.x.abs + velocity.y.abs + velocity.z.abs
+
+    def totalEnergy: Long = pot * kin
   }
 
   object Moon {
@@ -85,6 +100,7 @@ object Day12 extends Day {
       case MoonRegex(x, y, z) => Moon(Vector3d(x.toInt, y.toInt, z.toInt))
     }
   }
+
 }
 
 object Day12App extends App {
--- src/test/scala/aof/Day12Test.scala
+++ src/test/scala/aof/Day12Test.scala
@@ -1,27 +1,22 @@
 package aof
 
 import aof.Day12.Moon
+import aof.Day12Test.MoonsEx1
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day12Test extends AnyFunSuite {
 
   test("step") {
-
-    val moons = Day12Test.Ex1.map(Moon.from)
-
-    println("Step 0")
-    println(Day12Test.moonsToString(moons))
-    println("Step 1")
-    println(Day12Test.moonsToString(Day12.step(moons)))
-
-    println("Step 1")
-    println(Day12Test.moonsToString(Day12.steps(moons, 1)))
-
-    println("Step 10")
-    val ms = Day12.steps(moons, 10)
-    println(Day12Test.moonsToString(ms))
+    val ms = Day12.steps(MoonsEx1, 10)
     assert(Day12.totalEnergy(ms) == 179)
+  }
 
+  test("reachInitialState ex1") {
+    assert(Day12.reachInitialState(Day12Test.MoonsEx1) == 2772)
+  }
+
+  test("reachInitialState ex2") {
+    assert(Day12.reachInitialState(Day12Test.MoonsEx2) == 4_686_774_924L)
   }
 
 }
@@ -36,5 +31,16 @@ object Day12Test {
     "<x=4, y=-8, z=8>",
     "<x=3, y=5, z=-1>"
   )
+
+  val Ex2 = List(
+    "<x=-8, y=-10, z=0>",
+    "<x=5, y=5, z=10>",
+    "<x=2, y=-7, z=3>",
+    "<x=9, y=-8, z=-3>"
+  )
+
+  val MoonsEx1 = Day12Test.Ex1.map(Moon.from)
+
+  val MoonsEx2 = Day12Test.Ex2.map(Moon.from)
 
 }
