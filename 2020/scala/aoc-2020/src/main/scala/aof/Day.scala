31870c66d19d4cf2171331307f999ae0df7236a2
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-11-30 22:15:27 +0100

    aoc 2020

---
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day.scala
@@ -0,0 +1,22 @@
+package aof
+
+import scala.io.Source
+
+trait Day {
+
+  val day: String
+
+  private lazy val rs = getClass.getClassLoader.getResource(s"input/day$day.txt")
+
+  lazy val lines = Source.fromFile(rs.toURI).getLines().toList
+
+  def solutionPartA: String
+
+  def solutionPartB: String
+
+  def run(): Unit = {
+    println("SolutionPartA: " + solutionPartA)
+    println("SolutionPartB: " + solutionPartB)
+  }
+}
+
