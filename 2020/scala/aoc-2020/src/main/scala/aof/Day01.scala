b486f9ca99a102eae9df3c60fb4180fbd1d534ff
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-01 10:57:45 +0100

    day1 solved

--- 2020/scala/aoc-2020/src/main/scala/aof/Day01.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day01.scala
@@ -4,9 +4,24 @@ object Day01 extends Day with App {
 
   val day: String = "day01.txt"
 
-  def solutionPartA: String = ""
+  val xs = lines.map(_.toInt)
 
-  def solutionPartB: String = ""
+  def solutionPartA: String =  {
+    val ys = for {
+     x <- xs
+     y <- xs if x +y == 2020
+    } yield x * y
+    ys.head.toString
+  }
+
+  def solutionPartB: String =  {
+    val ys = for {
+      x <- xs
+      y <- xs
+      z <- xs if x + y + z == 2020
+    } yield x * y * z
+    ys.head.toString
+  }
 
   run()
 }
