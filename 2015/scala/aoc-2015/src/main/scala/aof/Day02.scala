3e17811185cd7d16594f894a7fc5f523e956ca1b
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-12-06 00:14:33 +0100

    day 2 solved

--- 2015/scala/aoc-2015/src/main/scala/aof/Day02.scala
+++ 2015/scala/aoc-2015/src/main/scala/aof/Day02.scala
@@ -4,12 +4,24 @@ object Day02 extends Day with App {
 
   val day: String = "day02.txt"
 
+  val dims = lines.map(_.split('x').map(_.toInt).toList)
+
+  def areas(l: Int, w: Int, h: Int): List[Int] = List(l * w, w * h, h * l)
+
   def solutionPartA: String = {
-    ""
+    val xs = dims.map {
+      case l :: w :: h :: Nil =>
+        val as = areas(l, w, h)
+        as.foldLeft(0)(_ + 2 * _) + as.min
+    }
+    xs.sum.toString
   }
 
   def solutionPartB: String = {
-    ""
+    val xs = dims.map { dim =>
+      dim.sorted.take(2).sum * 2 + dim.product
+    }
+    xs.sum.toString
   }
 
   run()
