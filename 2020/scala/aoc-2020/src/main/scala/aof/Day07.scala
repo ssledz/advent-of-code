2a9ce36aa11d26cff4b383a9edaba55343abea19
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-12-06 13:47:28 +0100

    added template for day 7

---
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day07.scala
@@ -0,0 +1,14 @@
+package aof
+
+object Day07 extends Day with App {
+
+  val day: String = "day07.txt"
+
+  def solutionPartA: String =
+    ""
+
+  def solutionPartB: String =
+    ""
+
+  run()
+}
