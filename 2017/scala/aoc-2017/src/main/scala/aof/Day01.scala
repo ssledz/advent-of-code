4b11ddbf2908c3ed5b6fa0170a33126afba43c80
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-09 00:52:52 +0100

    day 1

--- 2017/scala/aoc-2017/src/main/scala/aof/Day01.scala
+++ 2017/scala/aoc-2017/src/main/scala/aof/Day01.scala
@@ -4,12 +4,14 @@ object Day01 extends Day with App {
 
   val day: String = "day01.txt"
 
-  def solutionPartA: String = {
-    ""
-  }
+  val digits = lines.head.map(_ - '0').toList
 
-  def solutionPartB: String = {
-    ""
+  def calc(pairs: List[(Int, Int)]): Int = pairs.filter { case (f, s) => f == s }.map(_._1).sum
+
+  def solutionPartA: String = calc(digits.zip(digits.drop(1) ::: List(digits.head))).toString
+
+  def solutionPartB: String = digits.splitAt(digits.length / 2) match {
+    case (l1, l2) => calc(digits.zip(l2 ::: l1)).toString
   }
 
   run()
