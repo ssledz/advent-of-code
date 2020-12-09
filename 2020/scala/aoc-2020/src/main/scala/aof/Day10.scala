85015b9a975a520e5d63a79d23abb661424cb925
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-09 15:36:46 +0100

    init day 10

---
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day10.scala
@@ -0,0 +1,16 @@
+package aof
+
+object Day10 extends Day with App {
+
+  val day: String = "day10.txt"
+
+  def solutionPartA: String = {
+    ""
+  }
+
+  def solutionPartB: String = {
+    ""
+  }
+
+  run()
+}
