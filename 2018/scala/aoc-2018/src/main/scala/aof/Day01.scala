41ef7dee754e46ebafaeb8674a9cf28af546109f
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-09 02:03:30 +0100

    solved day 1

--- 2018/scala/aoc-2018/src/main/scala/aof/Day01.scala
+++ 2018/scala/aoc-2018/src/main/scala/aof/Day01.scala
@@ -4,12 +4,18 @@ object Day01 extends Day with App {
 
   val day: String = "day01.txt"
 
-  def solutionPartA: String = {
-    ""
-  }
+  val freqs = lines.map(_.toInt)
+
+  def solutionPartA: String = freqs.sum.toString
 
   def solutionPartB: String = {
-    ""
+    lazy val infFreqs: LazyList[Int] = LazyList.from(freqs) #::: infFreqs
+    infFreqs
+      .scanLeft((Set.empty[Int], 0, true)) { case ((xs, acc, _), e) => (xs + acc, acc + e, !xs.contains(acc)) }
+      .takeWhile(_._3)
+      .last
+      ._2
+      .toString
   }
 
   run()
