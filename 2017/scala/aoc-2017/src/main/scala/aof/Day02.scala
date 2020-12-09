4d80c1610147f8b72d6d99329a6cd3492bc958ce
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-09 01:13:43 +0100

    day 2

--- 2017/scala/aoc-2017/src/main/scala/aof/Day02.scala
+++ 2017/scala/aoc-2017/src/main/scala/aof/Day02.scala
@@ -4,12 +4,16 @@ object Day02 extends Day with App {
 
   val day: String = "day02.txt"
 
-  def solutionPartA: String = {
-    ""
-  }
+  val digits = lines.map(_.split(raw"""\s+""").map(_.toInt).toList)
+
+  def solutionPartA: String = digits.map(r => r.max - r.min).sum.toString
 
   def solutionPartB: String = {
-    ""
+    val sorted = digits.map(_.sorted(Ordering[Int].reverse))
+    val dividable = sorted.flatMap { xs =>
+      xs.zipWithIndex.flatMap { case (x, i) => xs.drop(i + 1).find(y => x % y == 0).map(x -> _) }
+    }
+    dividable.map { case (f, s) => f / s }.sum.toString
   }
 
   run()
