8cac09d10533fc3adb423c0669334547e77b9221
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-12 00:19:01 +0100

    refactored

--- 2020/scala/aoc-2020/src/main/scala/aof/Day11.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day11.scala
@@ -4,6 +4,8 @@ object Day11 extends Day with App {
 
   type Point = (Int, Int)
 
+  type Rule = (Map[Point, Char], Point) => Char => Char
+
   val day: String = "day11.txt"
 
   val area = lines.map(_.toList.zipWithIndex).zipWithIndex.flatMap { case (ys, y) => ys.map { case (c, x) => ((x, y), c) } }.toMap
@@ -28,11 +30,7 @@ object Day11 extends Day with App {
   def newRule2(adjacent: Point => List[Point])(area: Map[Point, Char], p: Point)(c: Char): Char =
     if (c == '#' && adjacent(p).count(p => area(p) == '#') >= 4) 'L' else c
 
-  def solutionPartA: String = {
-    val getAdjacent = adjacent(w, h) _
-    val rule1 = newRule1(getAdjacent) _
-    val rule2 = newRule2(getAdjacent) _
-
+  def simulate(area: Map[Point, Char], rule1: Rule, rule2: Rule): Int = {
     // area -> changed
     def iter(area: Map[Point, Char]): (Map[Point, Char], Boolean) = {
       val (newArea, changed) = indices.foldLeft((List.empty[(Point, Char)], false)) {
@@ -40,33 +38,22 @@ object Day11 extends Day with App {
           val rule = rule1(area, p).andThen(rule2(area, p))
           val c = area(p)
           val newC = rule(c)
-
-//          println(s"($p)\t\t$c\t->\t$newC\t\trule1: ${rule1(area, p)(c)}\t\trule2: ${newC}")
-
           ((p, newC) :: acc, changed || newC != c)
       }
       (newArea.toMap, changed)
     }
-
     def go(area: Map[Point, Char]): Map[Point, Char] = {
       val (newArea, changed) = iter(area)
       if (changed) go(newArea) else newArea
     }
-
     val stabilizedArea = go(area)
-    stabilizedArea.values.count(_ == '#').toString
-
-//    val (newArea, changed) = iter(area)
-//    val (newArea2, changed2) = iter(newArea)
-//    println(show(area, w, h))
-//    println(s"\nchanged: $changed\n")
-//    println(show(newArea, w, h))
-//    println(s"\nchanged: $changed2\n")
-//    println(show(newArea2, w, h))
-
+    stabilizedArea.values.count(_ == '#')
   }
 
-  def solutionPartB: String = ""
+  def solutionPartA: String = simulate(area, newRule1(adjacent(w, h)), newRule2(adjacent(w, h))).toString
+
+  def solutionPartB: String =
+    ""
 
   def show(area: Map[Point, Char], w: Int, h: Int): String = (0 until h).map(y => (0 until w).map(x => area(x -> y)).mkString).mkString("\n")
 
