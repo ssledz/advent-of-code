2738a271c104755d027640e0f1828de842541f48
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-12-15 17:54:15 +0100

    day 15 solved

--- 2020/scala/aoc-2020/src/main/scala/aof/Day15.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day15.scala
@@ -4,13 +4,19 @@ object Day15 extends Day with App {
 
   val day: String = "day15.txt"
 
-  def solutionPartA: String = {
-    ""
-  }
+  val numbers = lines.head.split(',').map(_.toLong).toList
 
-  def solutionPartB: String = {
-    ""
-  }
+  def playGame(mem: Map[Long, Int], last: Long, it: Int, end: Int): Long =
+    if (it - 1 == end) {
+      last
+    } else {
+      val newLast = if (mem.contains(last)) it - 1 - mem(last) else 0
+      playGame(mem.updated(last, it - 1), newLast, it + 1, end)
+    }
+
+  def solutionPartA: String = playGame((0L :: numbers).init.zipWithIndex.drop(1).toMap, numbers.last, numbers.length + 1, 2020).toString
+
+  def solutionPartB: String = playGame((0L :: numbers).init.zipWithIndex.drop(1).toMap, numbers.last, numbers.length + 1, 30000000).toString
 
   run()
 }
