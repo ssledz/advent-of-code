34f3d63b82cdbcee9d09a31aabaa429f0d39e829
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-12-20 01:15:28 +0100

    solved part 1

--- 2020/scala/aoc-2020/src/main/scala/aof/Day19.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day19.scala
@@ -2,7 +2,7 @@ package aof
 
 object Day19 extends Day with App {
 
-  val day: String = "day20.txt"
+  val day: String = "day19.txt"
 
   def parse: (Map[Int, Rule], List[String]) = {
     def go(xs: List[String], rules: List[(Int, Rule)]): (Map[Int, Rule], List[String]) = xs match {
@@ -17,23 +17,44 @@ object Day19 extends Day with App {
   rules.values.toList
 
   def expandRules(rules: Map[Int, Rule]): Map[Int, ExpandedRule] = {
-
-    def go(xs: List[DependentRule], acc: Map[Int, ExpandedRule]): Map[Int, ExpandedRule] = xs match {
+    def combine(xs: List[Set[String]]): List[String] = xs match {
+      case h :: Nil => h.toList
       case h :: t =>
-        ???
-      case Nil => acc
+        h.flatMap { x =>
+          combine(t).map(y => x + y)
+        }.toList
+      case Nil => List.empty
     }
-
-    val depRules = rules.values.collect { case r: DependentRule => r }.toList
-    val expanded = rules.collect { case (id, r: ExpandedRule) => (id, r) }
-    go(depRules, expanded)
+    def expand(id: Int): Set[String] = {
+      val rule = rules(id)
+      rule match {
+        case DependentRule(deps) =>
+          deps.flatMap { rules: List[Int] =>
+            val xs: List[Set[String]] = rules.map { rule =>
+              expand(rule)
+            }
+            combine(xs)
+          }.toSet
+        case ExpandedRule(value) => value
+      }
+    }
+    val depRules = rules.collect { case (id, r: DependentRule) => (id, r) }.toList
+    val expanded = rules.collect { case (id, r: ExpandedRule)  => (id, r) }.toList
+    depRules
+      .foldLeft(expanded) {
+        case (acc, (id, rule)) =>
+          val value = rule.deps.flatMap { rs =>
+            combine(rs.map(expand))
+          }
+          (id, ExpandedRule(value.toSet)) :: acc
+      }
+      .toMap
   }
 
   def solutionPartA: String = {
-    println(rules)
-    println
-    println(messages)
-    ""
+    val expanded = expandRules(rules)
+    val zeroRule = expanded(0)
+    messages.count(zeroRule.value.contains).toString
   }
 
   def solutionPartB: String =
