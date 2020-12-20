37eb3322921bdaea592215f7e6172ccc290b2b96
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-12-20 17:51:02 +0100

    solved part 1

--- 2020/scala/aoc-2020/src/main/scala/aof/Day20.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day20.scala
@@ -5,6 +5,7 @@ object Day20 extends Day with App {
   type Image = Vector[Vector[Char]]
 
   val day: String = "day20.txt"
+//  val day: String = "day21.txt"
 
   def parse: List[Tile] = {
     def go(xs: List[String], acc: List[Tile]): List[Tile] = xs match {
@@ -17,18 +18,62 @@ object Day20 extends Day with App {
     go(lines, List.empty)
   }
 
-  val tiles = parse
+  val tiles = parse.toSet
+
+  val edgeToTiles: Map[String, Set[Tile]] =
+    tiles
+      .flatMap(t => t.edges.map(_ -> t))
+      .groupBy(_._1)
+      .view
+      .mapValues(_.map(_._2))
+      .toMap
+
+  val mapTileEdges: Set[Tile] = edgeToTiles.filter(_._2.size == 1).flatMap(_._2).toSet
+
+  val mapEdges: Set[String] = edgeToTiles.filter(_._2.size == 1).keys.toSet
 
   def solutionPartA: String = {
-    println(tiles.map(_.toString).mkString("\n"))
-    ""
+//    println(mapTileEdges)
+//    println(tiles -- mapTileEdges)
+    val cornerTiles = mapTileEdges.filter { tile =>
+      val otherTiles: Set[Tile] = tiles - tile
+      val otherEdges = otherTiles.flatMap(_.edges)
+      val neighbours = tile.edges.filter(otherEdges.contains)
+
+//      println
+//      println("tile: " + tile)
+//      println("tile.edges: " + tile.edges)
+//      println("other tiles: " + otherTiles)
+//      println("neighbours: " + neighbours.flatMap(edgeToTiles.apply))
+//      println("neighbours: " + neighbours)
+//      println("neighbours cnt: " + neighbours.size)
+
+      tile.edges.count(otherEdges.contains) == 4
+//      tile.edges.count(mapEdges.contains) == 2
+    }
+    println("cornerTiles" + cornerTiles)
+    cornerTiles.map(_.id.toLong).product.toString
   }
 
   def solutionPartB: String = ""
 
   run()
 
-  case class Tile(id: Int, img: Image)
+  case class Tile(id: Int, img: Image) {
+
+    private def edgesFrom(img: Image) =
+      Set(
+        img.head.mkString,
+        img.head.reverse.mkString,
+        img.last.mkString,
+        img.last.reverse.mkString
+      )
+
+    val edges: Set[String] = edgesFrom(img) ++ edgesFrom(img.transpose)
+
+//    def innerImg: List[String]
+    override def toString: String = id.toString
+  }
   object Tile {
     val r = """Tile (\d+):""".r
     def from(xs: List[String]): (List[String], Tile) = {
