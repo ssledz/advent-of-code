ff01c5a00d563a047169af7e3c83d730fcbe71ef
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-12-25 00:31:22 +0100

    refactor

--- 2020/scala/aoc-2020/src/main/scala/aof/Day24.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day24.scala
@@ -15,7 +15,7 @@ object Day24 extends Day with App {
     ("ne", (1, -1))
   )
 
-  def solutionPartA: String = {
+  val blackTiles: List[(Int, Int)] = {
     @tailrec
     def traverse(xs: List[Char], pos: (Int, Int), acc: List[(Int, Int)]): List[(Int, Int)] =
       xs match {
@@ -23,11 +23,12 @@ object Day24 extends Day with App {
         case a :: b :: t                    => traverse(t, dirs(List(a, b).mkString) + pos, pos :: acc)
         case Nil                            => pos :: acc
       }
-
     val pos = lines.map(l => traverse(l.toList, (0, 0), List.empty).head)
     val xs = pos.groupBy(identity).view.mapValues(_.size).toMap
-    xs.count { case (_, cnt) => cnt % 2 == 1 }.toString
+    xs.filter { case (_, cnt) => cnt % 2 == 1 }.keys.toList
   }
+
+  def solutionPartA: String = blackTiles.size.toString
 
   def solutionPartB: String =
     ""
