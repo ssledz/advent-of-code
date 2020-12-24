afa0621bb673c467d069fc5e042c07bbe95ce4b5
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-12-25 00:55:31 +0100

    ...

--- 2020/scala/aoc-2020/src/main/scala/aof/Day24.scala
+++ 2020/scala/aoc-2020/src/main/scala/aof/Day24.scala
@@ -4,6 +4,8 @@ import aof.utils._
 import scala.annotation.tailrec
 object Day24 extends Day with App {
 
+  type Point = (Int, Int)
+
   val day: String = "day24.txt"
 
   val dirs = Map(
@@ -15,9 +17,9 @@ object Day24 extends Day with App {
     ("ne", (1, -1))
   )
 
-  val blackTiles: List[(Int, Int)] = {
+  val blackTiles: List[Point] = {
     @tailrec
-    def traverse(xs: List[Char], pos: (Int, Int), acc: List[(Int, Int)]): List[(Int, Int)] =
+    def traverse(xs: List[Char], pos: Point, acc: List[Point]): List[Point] =
       xs match {
         case h :: t if h == 'e' || h == 'w' => traverse(t, dirs(h.toString) + pos, pos :: acc)
         case a :: b :: t                    => traverse(t, dirs(List(a, b).mkString) + pos, pos :: acc)
@@ -30,8 +32,31 @@ object Day24 extends Day with App {
 
   def solutionPartA: String = blackTiles.size.toString
 
-  def solutionPartB: String =
-    ""
+  def adjacentTiles(p: Point): List[Point] = ???
+
+  def solutionPartB: String = {
+    val day0 = blackTiles.toSet
+    def go(blackTiles: Set[Point]): Set[Point] = {
+      val space = blackTiles.flatMap(adjacentTiles) ++ blackTiles
+      space.flatMap { tile =>
+        val adj = adjacentTiles(tile)
+        val cnt = adj.count(blackTiles.contains)
+        if (blackTiles.contains(tile)) {
+          if (cnt == 0 || cnt > 2) List.empty else List(tile)
+        } else {
+          if (cnt == 2) List(tile) else List.empty
+        }
+      }
+    }
+
+    def goN(n: Int): Set[Point] =
+      (1 to n)
+        .foldLeft(day0) {
+          case (black, _) => go(black)
+        }
+
+    goN(100).size.toString
+  }
 
   run()
 }
