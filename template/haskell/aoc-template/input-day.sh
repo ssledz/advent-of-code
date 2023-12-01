5eb3dc77c013c05635fa78124022735bfbbcdac0
Sławomir Śledź <slawomir.sledz@gmail.com>
2023-12-01 15:59:46 +0100

    Advent Of Code 2023

---
+++ template/haskell/aoc-template/input-day.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+day=$(echo "0$1" | fold -w1 | tail -2 | tr -d '\n')
+
+echo "day: $day"
+
+curl "https://adventofcode.com/2022/day/$1/input" \
+    -H "cookie: session=$SESSION" \
+    > input/day$day.txt
---
+++ template/haskell/aoc-template/src/Aoc.hs
@@ -0,0 +1,24 @@
+module Aoc
+  ( readLines
+  , sortDesc
+  , splitOn
+  , headMay
+  ) where
+
+import Data.List
+
+readLines :: FilePath -> ([String] -> String) -> IO ()
+readLines fileName f = do
+  content <- readFile fileName
+  putStrLn $ f (lines content)
+
+sortDesc :: Ord a => [a] -> [a]
+sortDesc = sortBy (flip compare)
+
+splitOn :: Char -> String -> (String, String)
+splitOn c s = let (l, r) = span (/= c) s
+              in (l, tail r)
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay xs = Just $ head xs
