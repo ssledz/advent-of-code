0b25eb8eae0aed0c400f999c71f5b4d9a241ad28
Sławomir Śledź <slawomir.sledz@gmail.com>
2023-12-02 14:43:16 +0100

    Solution day 01 part B

--- 2023/haskell/aoc-2023/app/day01.hs
+++ 2023/haskell/aoc-2023/app/day01.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import Aoc
-import Data.Char as Char
+import qualified Data.Char as Char
 import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.List as List
 
 main :: IO ()
 main = do
   run "input/day01.txt"
-  -- run "input/dayXX.txt"
 
 run :: FilePath -> IO ()
 run filePath = do
@@ -18,15 +20,47 @@ toDigit :: Char -> Maybe Int
 toDigit c | Char.isDigit c = Just $ Char.digitToInt c
           | otherwise = Nothing
 
-digits :: [String] -> [[Int]]
-digits lines = map (mapMaybe toDigit) lines
+digits :: String -> [Int]
+digits = mapMaybe toDigit
 
 numbers :: [[Int]] -> [Int]
 numbers = map (\xs -> (head xs) * 10 + last xs)
 
+digitsMap :: Map String Int
+digitsMap = Map.fromList
+  [ ("one", 1)
+  , ("two", 2)
+  , ("three", 3)
+  , ("four", 4)
+  , ("five", 5)
+  , ("six", 6)
+  , ("seven", 7)
+  , ("eight", 8)
+  , ("nine", 9)
+  ]
+
+lookupDigit :: String -> Maybe Int
+lookupDigit xs = do
+  phrase <- List.find (\x -> x `List.isPrefixOf` xs) (Map.keys digitsMap)
+  digit  <- Map.lookup phrase digitsMap
+  return digit
+
+toDigit' :: String -> (Maybe Int, String)
+toDigit' [] = (Nothing, [])
+toDigit' cs@(c:rest) | Char.isDigit c = (toDigit c, rest)
+                     | otherwise = (lookupDigit cs, rest)
+
+digits' :: String -> [Int]
+digits' = go []
+  where
+    go acc [] = reverse acc
+    go acc xs =
+      case toDigit' xs of
+        (Just d, rest)  -> go (d : acc) rest
+        (Nothing, rest) -> go acc rest
+
 partA :: [String] -> String
-partA = show . sum . numbers . digits
--- partA = show  . digits
+partA = show . sum . numbers . map digits
 
 partB :: [String] -> String
-partB = show . length
+partB = show . sum . numbers . map digits'
