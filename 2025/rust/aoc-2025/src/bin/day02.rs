eee0dcdfcced932d8ef6ad1e752517a214e4979e
Sławomir Śledź <slawomir.sledz@gmail.com>
2026-01-14 16:22:38 +0100

    solution day02 part a

--- 2025/rust/aoc-2025/src/bin/day02.rs
+++ 2025/rust/aoc-2025/src/bin/day02.rs
@@ -1,19 +1,48 @@
+use aoc_2025::num_to_digits;
 use aoc_2025::read_line;
+use std::ops::RangeInclusive;
 
 fn main() {
     println!("Solution part_a: {}", part_a());
     println!("Solution part_b: {}", part_b());
 }
 
-fn is_invalid_id(id: &str) -> bool {
-    true
+fn to_range(s: &str) -> RangeInclusive<i64> {
+    let parts: Vec<&str> = s.split('-').collect();
+    let start = parts[0].parse().unwrap();
+    let end = parts[1].parse().unwrap();
+    start..=end
 }
 
-fn part_a() -> i32 {
-    let line = read_line("input/day02.txt");
-    let id_ranges: Vec<&str> = line.split(',').collect();
+fn is_invalid_id(id: &i64) -> bool {
+    let digits: Vec<i32> = num_to_digits(id);
+    if digits[0] == 0 {
+        return true;
+    }
 
-    -1
+    let len = digits.len();
+
+    if len % 2 == 0 {
+        let left: Vec<&i32> = digits.iter().take(len / 2).collect();
+        let right: Vec<&i32> = digits.iter().skip(len / 2).collect();
+        return left == right;
+    }
+
+    false
+}
+
+fn part_a() -> i64 {
+    let line = read_line("input/day02.txt");
+    let id_ranges: Vec<RangeInclusive<i64>> = line.split(',').map(|s| to_range(s)).collect();
+
+    let invalid_ids: Vec<i64> = id_ranges
+        .into_iter()
+        .flat_map(|r| r.into_iter().filter(|id| is_invalid_id(id)))
+        .collect();
+
+    // println!("{:?}", invalid_ids);
+
+    invalid_ids.iter().sum()
 }
 
 fn part_b() -> i32 {
--- 2025/rust/aoc-2025/src/lib.rs
+++ 2025/rust/aoc-2025/src/lib.rs
@@ -1,3 +1,4 @@
+use num::Num;
 use std::fs;
 
 pub fn read_lines(path: &str) -> Vec<String> {
@@ -6,4 +7,11 @@ pub fn read_lines(path: &str) -> Vec<String> {
 
 pub fn read_line(path: &str) -> String {
     fs::read_to_string(path).expect(&format!("Error reading file {path}"))
+}
+
+pub fn num_to_digits<T: Num + ToString>(n: &T) -> Vec<i32> {
+    n.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .collect()
 }
