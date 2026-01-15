189ff101c581ceeda4586f44e2ae9662b57a8e6f
Sławomir Śledź <slawomir.sledz@gmail.com>
2026-01-15 19:41:59 +0100

    solution day02 part b

--- 2025/rust/aoc-2025/src/bin/day02.rs
+++ 2025/rust/aoc-2025/src/bin/day02.rs
@@ -14,7 +14,7 @@ fn to_range(s: &str) -> RangeInclusive<i64> {
     start..=end
 }
 
-fn is_invalid_id(id: &i64) -> bool {
+fn is_invalid_id_part1(id: &i64) -> bool {
     let digits: Vec<i32> = num_to_digits(id);
     if digits[0] == 0 {
         return true;
@@ -31,8 +31,10 @@ fn is_invalid_id(id: &i64) -> bool {
     false
 }
 
-fn part_a() -> i64 {
-    let line = read_line("input/day02.txt");
+fn compute_invalid_ids<F>(line: &str, is_invalid_id: F) -> Vec<i64>
+where
+    F: Fn(&i64) -> bool,
+{
     let id_ranges: Vec<RangeInclusive<i64>> = line.split(',').map(|s| to_range(s)).collect();
 
     let invalid_ids: Vec<i64> = id_ranges
@@ -40,12 +42,39 @@ fn part_a() -> i64 {
         .flat_map(|r| r.into_iter().filter(|id| is_invalid_id(id)))
         .collect();
 
+    invalid_ids
+}
+
+fn is_invalid_id_part2(id: &i64) -> bool {
+    let digits: Vec<i32> = num_to_digits(id);
+
+    for i in 1..=digits.len() / 2 {
+        let pattern = &digits[0..i];
+        if digits.len() % pattern.len() == 0 {
+            let illegal = pattern.repeat(digits.len() / pattern.len());
+            if illegal == digits {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn part_a() -> i64 {
+    let line = read_line("input/day02.txt");
+
+    let invalid_ids = compute_invalid_ids(&line, is_invalid_id_part1);
+
     // println!("{:?}", invalid_ids);
 
     invalid_ids.iter().sum()
 }
 
-fn part_b() -> i32 {
+fn part_b() -> i64 {
     let line = read_line("input/day02.txt");
-    -1
+
+    let invalid_ids = compute_invalid_ids(&line, is_invalid_id_part2);
+
+    invalid_ids.iter().sum()
 }
