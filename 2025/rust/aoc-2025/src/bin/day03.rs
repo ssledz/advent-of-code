bd92c0221b4d9e454b216ecd6d75cc9b13551132
Sławomir Śledź <slawomir.sledz@gmail.com>
2026-01-17 16:58:00 +0100

    Add solution part b for day03

---
+++ 2025/rust/aoc-2025/src/bin/day03.rs
@@ -0,0 +1,44 @@
+use aoc_2025::read_lines;
+
+fn main() {
+    println!("Solution part_a: {}", part_a());
+    println!("Solution part_b: {}", part_b());
+}
+
+fn largest_joltage(jr: &Vec<i32>) -> i64 {
+    let (idx, a) = jr[..jr.len() - 1]
+        .iter()
+        .enumerate()
+        .max_by(|a, b| {
+            let tmp = a.1.cmp(b.1);
+            if tmp == std::cmp::Ordering::Equal {
+                a.0.cmp(&b.0).reverse()
+            } else {
+                tmp
+            }
+        })
+        .unwrap();
+    let b = jr[idx + 1..].iter().max().unwrap();
+    (*a as i64) * 10 + (*b as i64)
+}
+
+fn part_a() -> i64 {
+    let lines = read_lines("input/day03.txt");
+    let jrs: Vec<Vec<i32>> = lines
+        .into_iter()
+        .map(|line| {
+            line.chars()
+                .into_iter()
+                .map(|c| c.to_digit(10).unwrap() as i32)
+                .collect()
+        })
+        .collect();
+
+    let largest: Vec<i64> = jrs.into_iter().map(|jr| largest_joltage(&jr)).collect();
+
+    largest.into_iter().sum()
+}
+
+fn part_b() -> i64 {
+    -1
+}
